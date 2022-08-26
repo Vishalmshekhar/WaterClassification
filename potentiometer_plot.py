a8edd0d9c1ecc3bec68ea4b8e087379f241732ef
Vishalmshekhar <78576906+Vishalmshekhar@users.noreply.github.com>
2022-08-26 23:57:33 +0530

    Create potentiometer_plot.py

---
+++ potentiometer_plot.py
@@ -0,0 +1,26 @@
+# potentiometer_plot.py
+
+import serial
+import time
+import matplotlib.pyplot as plt
+
+# make sure the 'COM#' is set according the Windows Device Manager
+ser = serial.Serial('COM4', 9800, timeout=1)
+time.sleep(2)
+
+data = []
+for i in range(50):
+    line = ser.readline()   # read a byte string
+    if line:
+        string = line.decode()  # convert the byte string to a unicode string
+        num = int(string) # convert the unicode string to an int
+        print(num)
+        data.append(num) # add int to data list
+ser.close()
+
+# build the plot
+plt.plot(data)
+plt.xlabel('Time')
+plt.ylabel('Potentiometer Reading')
+plt.title('Potentiometer Reading vs. Time')
+plt.show()
